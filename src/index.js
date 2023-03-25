89c00d529a600be4098107c55a53443f0cec34af
yazeedahmed <dee3ayahmed25@gmail.com>
2023-03-25 13:11:50 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector("#create-task-form");
+const taskList = document.querySelector("#tasks");
+
+
+form.addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  
+  const newTaskInput = document.querySelector("#new-task-description");
+  const newTaskDescription = newTaskInput.value;
+
+  
+  newTaskInput.value = "";
+
+  const newTaskListItem = document.createElement("li");
+  newTaskListItem.innerText = newTaskDescription;
+
+  
+  taskList.appendChild(newTaskListItem);
 });
